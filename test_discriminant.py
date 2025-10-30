b37db7a4b7d3888c428874b7f5e3d03dd5f16ae7
Enne678 <126559456+Enne678@users.noreply.github.com>
2025-10-31 03:04:38 +0700

    Fix import and function name for discriminant tests

--- test_discriminant.py
+++ test_discriminant.py
@@ -1,14 +1,14 @@
 import unittest
-from discriminant import calculate_driminant
+from discriminant import calculate_discriminant
 
 
 class TestDiscriminant(unittest.TestCase):
 
     def test_positive_discriminant(self):
-        self.assertEqual(calculate_driminant(1, 5, 6), 1)
+        self.assertEqual(calculate_discriminant(1, 5, 6), 1)
 
     def test_zero_discriminant(self):
-        self.assertEqual(calculate_driminant(1, 2, 1), 0)
+        self.assertEqual(calculate_discriminant(1, 2, 1), 0)
 
     def test_negative_discriminant(self):
-        self.assertEqual(calculate_driminant(1, 1, 1), -3)
+        self.assertEqual(calculate_discriminant(1, 1, 1), -3)
