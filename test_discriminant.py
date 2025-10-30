6e82b643cc7099940236d84a03816cbf723b82ee
Enne678 <126559456+Enne678@users.noreply.github.com>
2025-10-31 02:53:40 +0700

    Update test_discriminant.py

--- test_discriminant.py
+++ test_discriminant.py
@@ -1,17 +1,14 @@
 import unittest
-from discriminant import calculate_discriminant
+from discriminant import calculate_driminant
+
 
 class TestDiscriminant(unittest.TestCase):
-    
+
     def test_positive_discriminant(self):
-        # D > 0 - два корня
-        self.assertEqual(calculate_discriminant(1, 5, 6), 1)
-        self.assertEqual(calculate_discriminant(2, 7, 3), 25)
-    
+        self.assertEqual(calculate_driminant(1, 5, 6), 1)
+
     def test_zero_discriminant(self):
-        # D = 0 - один корень
-        self.assertEqual(calculate_discriminant(1, 2, 1), 0)
-    
+        self.assertEqual(calculate_driminant(1, 2, 1), 0)
+
     def test_negative_discriminant(self):
-        # D < 0 - нет действительных корней
-        self.assertEqual(calculate_discriminant(1, 1, 1), -3)
+        self.assertEqual(calculate_driminant(1, 1, 1), -3)
