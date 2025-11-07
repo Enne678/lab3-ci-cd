a87edd3a37247a9cc40ee00c200b7585a974b1fd
Enne678 <126559456+Enne678@users.noreply.github.com>
2025-11-07 15:11:28 +0700

    Update test_discriminant.py

--- test_discriminant.py
+++ test_discriminant.py
@@ -3,16 +3,16 @@ from discriminant import calculate_discriminant
 
 
 class TestDiscriminant(unittest.TestCase):
-    
+
     # Положительные тесты (D >= 0)
     def test_positive_discriminant(self):
         result = calculate_discriminant(1, 5, 6)
         self.assertEqual(result, 1)
-    
+
     def test_zero_discriminant(self):
         result = calculate_discriminant(1, 2, 1)
         self.assertEqual(result, 0)
-    
+
     # Негативные тесты (D < 0)
     def test_negative_discriminant(self):
         result = calculate_discriminant(1, 1, 1)
@@ -21,4 +21,3 @@ class TestDiscriminant(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
