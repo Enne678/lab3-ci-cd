0f6d2fb148a220d2c4ac9134f2f4c603bc11fe9f
Enne678 <126559456+Enne678@users.noreply.github.com>
2025-11-07 15:06:32 +0700

    Update discriminant.py

--- discriminant.py
+++ discriminant.py
@@ -1,7 +1,9 @@
+import math
+
+
 def calculate_discriminant(a, b, c):
     """
     Вычисляет дискриминант квадратного уравнения ax^2 + bx + c = 0
     Формула: D = b^2 - 4ac
     """
     return b * b - 4 * a * c
-    
